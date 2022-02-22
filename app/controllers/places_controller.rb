f866dbf4275aa8d8284d529fbfe0b68505a0a5f7
jstone5 <john.stone@kellogg.northwestern.edu>
2022-02-22 17:08:20 +0000

    added new.html.erb and show.html.erb; created "Add Place" button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,15 +4,15 @@ class PlacesController < ApplicationController
         @places = Place.all
     end
     
-    # def new
-    #     @place = Place.new
-    # end
+    def new
+        @place = Place.new
+    end
     
-    # def create
-    #     values = params["place"]
-    #     @place = Place.new(values)
-    #     @place.save
-    #     redirect_to "/places"
-    # end
+    def create
+        values = params["place"]
+        @place = Place.new(values)
+        @place.save
+        redirect_to "/places"
+    end
 
 end
