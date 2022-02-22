017dc07564960af644908cf885dd02ecba4bed74
jstone5 <john.stone@kellogg.northwestern.edu>
2022-02-22 17:01:17 +0000

    added places controller, created index.html.erb

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,18 @@
+class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+    end
+    
+    # def new
+    #     @place = Place.new
+    # end
+    
+    # def create
+    #     values = params["place"]
+    #     @place = Place.new(values)
+    #     @place.save
+    #     redirect_to "/places"
+    # end
+
+end
